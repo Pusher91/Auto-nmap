042efa1e714e09c0dc6552c6db50a8108c6a3abe
pusher91 <dave.oren91@gmail.com>
2021-11-20 23:02:39 -0500

    update

--- autoNmap.py
+++ autoNmap.py
@@ -14,7 +14,6 @@ def tcp_ping_scan():
     tcp_ping_scan_check = subprocess.call("test -e '{}'".format('tcp_ping_scan'), shell=True)
 
     if tcp_ping_scan_check != 0:
-
         print("Scanning for open tcp ports...")
         subprocess.run(["nmap", "-p-","{}".format(target), "-oN", "tcp_ping_scan"], stdout=subprocess.DEVNULL)
         print("+ Finished scanning for open ports. 'tcp_ping_scan' created.")
@@ -27,7 +26,6 @@ def tcp_script_scan():
     tcp_script_scan_check = subprocess.call("test -e '{}'".format('tcp_script_scan'), shell=True)
 
     if tcp_script_scan_check != 0:
-
         print("Running nmap scripts against open tcp ports...")
 
         textfile = open("tcp_ping_scan", "r")
@@ -35,9 +33,9 @@ def tcp_script_scan():
         textfile.close()
         matches = re.findall("[0-9].*/tcp", filetext)
         matches2 = re.findall("\d{1,5}[0-9]", str(matches))
-
         open_tcp_ports = ",".join(matches2)
         
+        #nmap -p <open ports> -sV -sC -oN tcp_script_scan
         subprocess.run(["nmap", "-p","{}".format(open_tcp_ports), "{}".format(target), "-sV", "-sC", "-oN", "tcp_script_scan"], stdout=subprocess.DEVNULL)
         print("+ Finished tcp script scan. 'tcp_script_scan' created.")
     
@@ -49,9 +47,7 @@ def udp_ping_scan():
     udp_ping_scan_check = subprocess.call("test -e '{}'".format('udp_ping_scan'), shell=True)
     
     if udp_ping_scan_check != 0:
-
         print("Scanning for open udp ports...")
-
         subprocess.run(["nmap", "-sU", "-sV","{}".format(target), "-oN", "udp_ping_scan"], stdout=subprocess.DEVNULL)
         print("+ Finished udp ping scan. 'udp_ping_scan' created.")
     
@@ -63,7 +59,6 @@ def udp_script_scan():
     udp_script_scan_check = subprocess.call("test -e '{}'".format('udp_script_scan'), shell=True)
     
     if udp_script_scan_check != 0:
-
         print("Running nmap scripts against open udp ports...")
 
         textfile = open("udp_ping_scan", "r")
@@ -71,11 +66,10 @@ def udp_script_scan():
         textfile.close()
         matches = re.findall("[0-9].*/udp", filetext)
         matches2 = re.findall("\d{1,5}[0-9]", str(matches))
-
         open_udp_ports = ",".join(matches2)
 
+        #nmap -p <open ports> -sU -sV -sC -oN udp_script_scan
         subprocess.run(["nmap", "-p","{}".format(open_udp_ports), "{}".format(target), "-sU", "-sV", "-sC", "-oN", "udp_script_scan"], stdout=subprocess.DEVNULL)
-
         print("+ Finished udp script scan. 'udp_script_scan' created.")
     
     elif udp_script_scan_check == 0:
